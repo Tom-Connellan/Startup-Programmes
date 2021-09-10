553f4a0bcf11e0129146ef2a04476be4ba3184fc
Tom-Connellan <85558098+Tom-Connellan@users.noreply.github.com>
2021-09-10 12:14:42 +0100

    Names - Task1
    This it the first task

---
+++ 1.Names.py
@@ -0,0 +1,12 @@
+#Task 1
+
+#inputting their first name
+fname = input("Input your first name")
+
+#inputting their second name
+sname = input("Input your last name")
+
+#concatinating the 2 names
+fullname = fname,sname
+
+print(fullname)

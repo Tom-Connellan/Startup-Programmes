263f712668367f54062d2ce2aaca0a4c7e50aa35
Tom-Connellan <85558098+Tom-Connellan@users.noreply.github.com>
2021-09-11 18:47:35 +0100

    4.Remainder.py
    Inputs and integer and a divider and produces the integer and remainder

---
+++ 4.Remainder.py
@@ -0,0 +1,10 @@
+#Task 4
+
+#Input the integer
+integer = int(input("Input the number to be divided: "))
+
+#Input the divisor
+divisor = int(input("Input the divisor: "))
+
+#calculate the number and remainder
+print(str(integer//divisor) + " remainder " + str(integer%divisor))

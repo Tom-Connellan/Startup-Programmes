e5843a113c5ca3011bce3ba14caa90998ad64afa
Tom-Connellan <85558098+Tom-Connellan@users.noreply.github.com>
2021-09-13 11:59:25 +0100

    7.TimesTable.py
    Asks the user to input a number then shows the times table between 1 and 10

---
+++ 7.TimesTable.py
@@ -0,0 +1,11 @@
+#Task 7
+
+#Ask the user for their number
+iNum = int(input("Enter a number between 1 and 10: "))
+
+#While loop
+counter = 1
+while counter < 11:
+    print(counter * iNum)
+    counter = counter + 1
+#endwhile

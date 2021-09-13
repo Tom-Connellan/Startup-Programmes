bbb1cda7957d0144be307919a50fe2c21bbdb2b2
Tom-Connellan <85558098+Tom-Connellan@users.noreply.github.com>
2021-09-13 11:51:09 +0100

    5.Hund,Ten,Int.py
    This programme gets a 3 digit number and splits it into its 3 parts

---
+++ 5.Hundreds, Ten, Units.py
@@ -0,0 +1,13 @@
+#Task 5
+
+#Input the number
+number = int(input("Please input an integer between 100 and 999: "))
+
+#Calculating each digit
+hund = number//100
+ten = number //10
+ten1 = ten%10
+dig = number%ten
+
+#showing the results
+print(hund, ten1, dig)

2ce456e4715e70caddfa87cecfbdfaa3941850db
Tom-Connellan <85558098+Tom-Connellan@users.noreply.github.com>
2021-09-16 20:10:30 +0100

    Create 11.Time,Time,Time.py

---
+++ 11.Time,Time,Time.py
@@ -0,0 +1,9 @@
+#Task 11
+
+time = str(input("Input the time (Hours, Minutes, Seconds): "))
+    
+hours, minutes, seconds = time.split(':')
+hours = int(hours) * 60 * 60
+minutes = int(minutes) * 60 * 60
+seconds = int(seconds)
+print(hours + minutes + seconds)
